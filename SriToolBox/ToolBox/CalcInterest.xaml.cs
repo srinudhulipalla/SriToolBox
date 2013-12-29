8bfd43ee8eeb94d6a6a42edd54e72252dea3b425
Srinivasa Rao Dhulipalla <srinudhulipalla@gmail.com>
2013-12-29 17:04:24 +0530

    calc interest impl

--- SriToolBox/ToolBox/CalcInterest.xaml.cs
+++ SriToolBox/ToolBox/CalcInterest.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Text.RegularExpressions;
 
 namespace SriToolBox.ToolBox
 {
@@ -17,8 +18,87 @@ namespace SriToolBox.ToolBox
     {
         public CalcInterest()
         {
-            InitializeComponent();
-            //test1
+            InitializeComponent();            
         }
+
+        private void BtnCalculte_Click(object sender, RoutedEventArgs e)
+        {
+            string errorMsg = string.Empty;
+
+            if (!ValidData(ref errorMsg))
+            {
+                MessageBox.Show(errorMsg, "Warning!", MessageBoxButton.OK);
+                return;
+            }
+
+            CompoundInterest Interest = new CompoundInterest();
+            Interest.Amount = double.Parse(txtAmount.Text);
+            Interest.InterestRate = double.Parse(txtInterest.Text);
+            Interest.Duration = double.Parse(txtDuration.Text);
+
+            switch (lstDuration.SelectedIndex)
+            {
+                case 0: Interest.DurationMethod = CompoundInterest.DurationValue.Day;
+                    break;
+                case 1: Interest.DurationMethod = CompoundInterest.DurationValue.Month;
+                    break;
+                case 2: Interest.DurationMethod = CompoundInterest.DurationValue.Year;
+                    break;
+            }
+
+            switch (lstFrequency.SelectedIndex)
+            {
+                case 0: Interest.Frequency = CompoundInterest.FrequencyValue.Monthly;
+                    break;
+                case 1: Interest.Frequency = CompoundInterest.FrequencyValue.Quarterly;
+                    break;
+                case 2: Interest.Frequency = CompoundInterest.FrequencyValue.HalfYearly;
+                    break;
+                case 3: Interest.Frequency = CompoundInterest.FrequencyValue.Yearly;
+                    break;
+            }
+
+            if (Interest.Calculate())
+            {
+                string message = "Amount Invested Rs.: " + Interest.Amount + Environment.NewLine + Environment.NewLine;
+                message += "Interest Earned Rs.: " + Interest.InterestEarned + Environment.NewLine;
+                message += "Maturity Value Rs.: " + Interest.MaturityAmount;
+
+                MessageBox.Show(message, "Done!", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show(Interest.LastError, "Error!", MessageBoxButton.OK);
+            }            
+        }
+
+        private void IconHome_Click(object sender, EventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+        private void IconClear_Click(object sender, EventArgs e)
+        {
+            txtAmount.Text = txtInterest.Text = txtDuration.Text = string.Empty;
+            lstDuration.SelectedIndex = lstFrequency.SelectedIndex = 1;
+        }
+
+        bool ValidData(ref string errorMsg)
+        {
+            Regex isNumber = new Regex(@"^[0-9]+(\.[0-9]+)?$");
+
+            if (!isNumber.IsMatch(txtAmount.Text))
+                errorMsg = "Principal Amount entered is Invalid";
+
+            else if (!isNumber.IsMatch(txtInterest.Text))
+                errorMsg = "Rate of Interest entered is Invalid";
+
+            else if (!isNumber.IsMatch(txtDuration.Text))
+                errorMsg = "Duration entered is Invalid";
+
+            return string.IsNullOrEmpty(errorMsg);
+        }
+
+        
     }
 }
---
+++ SriToolBox/ToolBox/ToolBox.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SriToolBox.ToolBox
+{
+    public class CompoundInterest
+    {
+        private double _amount;
+        private double _interestRate;
+        private double _duration;
+        private DurationValue _durationMethod;
+        private FrequencyValue _frequency;
+        private double _interestEarned;
+        private double _maturityAmount;
+        private string _lastError;
+
+        public enum DurationValue
+        {
+            Day = 365,
+            Month = 12,
+            Year = 1
+        }
+
+        public enum FrequencyValue
+        {
+            Monthly = 12,
+            Quarterly = 4,
+            HalfYearly = 2,
+            Yearly = 1
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public double InterestRate
+        {
+            get { return _interestRate; }
+            set { _interestRate = value; }
+        }
+
+        public double Duration
+        {
+            get { return _duration; }
+            set { _duration = value; }
+        }
+
+        public DurationValue DurationMethod
+        {
+            get { return _durationMethod; }
+            set { _durationMethod = value; }
+        }
+
+        public FrequencyValue Frequency
+        {
+            get { return _frequency; }
+            set { _frequency = value; }
+        }
+
+        public double InterestEarned
+        {
+            get { return _interestEarned; }
+        }
+
+        public double MaturityAmount
+        {
+            get { return _maturityAmount; }
+        }
+
+        public string LastError
+        {
+            get { return _lastError; }            
+        }
+
+        public bool Calculate()
+        {
+            try
+            {
+                double value1 = 1 + ((_interestRate / 100) / (int)_frequency);
+                double value2 = (int)_frequency * (_duration / (int)_durationMethod);
+                double value3 = Math.Pow(value1, value2);
+
+                _maturityAmount = Math.Round(_amount * value3, 2);
+                _interestEarned = Math.Round(_maturityAmount - _amount, 2);
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _lastError = ex.Message;
+                return false;
+            }
+        }
+
+    }
+}
